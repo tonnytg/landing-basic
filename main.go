99ef3f3d978ab83e159703632ac50fb794afb68a
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2024-03-13 00:21:44 +0000

    feat: add handler

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "teste")
+}
+
 func main() {
-	fmt.Println("Start")
+	log.Println("start webserver")
+
+	http.HandleFunc("/", IndexHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("http can't start", err)
+	}
 }
